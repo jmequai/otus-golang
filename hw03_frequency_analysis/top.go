1686458155ead2dea27ab0c908a8af7e3a651193
Крупин Денис <denis.krupin@vitech.team>
2023-12-19 21:12:30 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,75 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+const tokenLimit = 10
+
+var rgx = regexp.MustCompile(`(?:\pP*((?:\pP*\pL+)+)\pP*)|(\pP{2,})`)
+
+type token struct {
+	str   string
+	count int
+}
+
+type tokenList []*token
+
+func (tl tokenList) Len() int {
+	return len(tl)
+}
+
+func (tl tokenList) Less(i, j int) bool {
+	a := tl[i]
+	b := tl[j]
+
+	if a.count == b.count {
+		return a.str < b.str
+	}
+
+	return a.count > b.count
+}
+
+func (tl tokenList) Swap(i, j int) {
+	tl[i], tl[j] = tl[j], tl[i]
+}
+
+func Top10(s string) []string {
+	tokens := make(tokenList, 0)
+	pointers := make(map[string]*token)
+
+	for _, group := range rgx.FindAllStringSubmatch(s, -1) {
+		sub := group[2]
+
+		if sub == "" {
+			sub = strings.ToLower(group[1])
+		}
+
+		if _, ok := pointers[sub]; !ok {
+			t := token{sub, 1}
+
+			tokens = append(tokens, &t)
+			pointers[sub] = &t
+
+			continue
+		}
+
+		pointers[sub].count++
+	}
+
+	sort.Sort(tokens)
+
+	sorted := make([]string, 0)
+
+	for i, p := range tokens {
+		if i == tokenLimit {
+			break
+		}
+
+		sorted = append(sorted, p.str)
+	}
+
+	return sorted
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -78,5 +78,17 @@ func TestTop10(t *testing.T) {
 			}
 			require.Equal(t, expected, Top10(text))
 		}
+	})
+
+	t.Run("сyr test", func(t *testing.T) {
+		expected := []string{
+			"ю",
+			"яя",
+			"---",
+			"еще",
+			"ещё",
+			"ю-ю",
+		}
+		require.Equal(t, expected, Top10("'Яя'   -   яя, еще яЯ! +ЯЯ ещё. Ю \t --- ю , ю ю Ю-ю..."))
 	})
 }
