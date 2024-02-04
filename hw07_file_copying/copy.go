b247945af31eb16f60901f4be809d4ec5c541991
Крупин Денис <denis.krupin@vitech.team>
2024-02-04 22:24:45 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,82 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+
+	fBufSize int64 = 1024
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	f, fErrOpen := os.Open(fromPath)
+	if fErrOpen != nil {
+		return ErrUnsupportedFile
+	}
+
+	defer f.Close()
+
+	fstat, fErrStat := f.Stat()
+	if fErrStat != nil {
+		return ErrUnsupportedFile
+	}
+
+	fsize := fstat.Size()
+
+	if offset > fsize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || (fsize > 0 && offset+limit > fsize) {
+		limit = fsize - offset
+	}
+
+	_, fErrSeek := f.Seek(offset, 0)
+	if fErrSeek != nil {
+		return fErrSeek
+	}
+
+	t, tErrOpen := os.OpenFile(toPath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o644)
+	if tErrOpen != nil {
+		return tErrOpen
+	}
+
+	defer t.Close()
+
+	fProgress := NewProgress(limit)
+	defer fProgress.Finish()
+
+	fCopySize := int64(0)
+
+	for {
+		if fCopySize >= limit {
+			break
+		}
+
+		currentBufSize := limit - fCopySize
+
+		if currentBufSize > fBufSize {
+			currentBufSize = fBufSize
+		}
+
+		n, err := io.CopyN(t, f, currentBufSize)
+
+		fCopySize += n
+		fProgress.Add(n)
+		// time.Sleep(time.Second)
+
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return err
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 var (
 	from, to      string
-	limit, offset int64
+	limit, offset uint64
 )
 
 func init() {
 	flag.StringVar(&from, "from", "", "file to read from")
 	flag.StringVar(&to, "to", "", "file to write to")
-	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
-	flag.Int64Var(&offset, "offset", 0, "offset in input file")
+	flag.Uint64Var(&limit, "limit", 0, "limit of bytes to copy")
+	flag.Uint64Var(&offset, "offset", 0, "offset in input file")
 }
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+
+	if err := Copy(from, to, int64(offset), int64(limit)); err != nil {
+		fmt.Println(err)
+	}
 }
