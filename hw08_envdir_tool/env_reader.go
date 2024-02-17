fd8950dd27032aecb7e11a12388aa33d9d63c2eb
Крупин Денис <denis.krupin@vitech.team>
2024-02-17 21:15:00 +0300

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +23,68 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		return nil, fmt.Errorf("directory %q not exists", dir)
+	}
+
+	env := Environment(make(map[string]EnvValue))
+
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, e error) error {
+		if e != nil {
+			return e
+		}
+
+		if d.IsDir() || strings.ContainsRune(d.Name(), '=') {
+			return nil
+		}
+
+		file := filepath.Join(dir, d.Name())
+
+		val, err := readFile(file)
+		if err != nil {
+			return err
+		}
+
+		env[d.Name()] = EnvValue{
+			Value:      val,
+			NeedRemove: val == "",
+		}
+
+		return nil
+	})
+
+	return env, err
+}
+
+func readFile(file string) (string, error) {
+	f, fErrOpen := os.Open(file)
+	if fErrOpen != nil {
+		return "", fErrOpen
+	}
+
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+
+	val, fErrRead := r.ReadBytes('\n')
+	if fErrRead != nil {
+		if errors.Is(fErrRead, io.EOF) {
+			return convertValue(val), nil
+		}
+
+		return "", fErrRead
+	}
+
+	val = val[0 : len(val)-1]
+
+	return convertValue(val), nil
+}
+
+func convertValue(val []byte) string {
+	if len(val) > 0 {
+		val = bytes.ReplaceAll(val, []byte{0x00}, []byte{'\n'})
+		val = bytes.TrimRight(val, " \t")
+	}
+
+	return string(val)
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,62 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const DefaultErrCode = 128
+
+var ErrCmdRequired = errors.New("command required")
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		fmt.Println(ErrCmdRequired)
+		return DefaultErrCode
+	}
+
+	if err := initEnvironment(env); err != nil {
+		fmt.Println(err)
+		return DefaultErrCode
+	}
+
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	if err := command.Run(); err != nil {
+		var errExit *exec.ExitError
+
+		if errors.As(err, &errExit) {
+			return errExit.ExitCode()
+		}
+
+		fmt.Println(err)
+		return DefaultErrCode
+	}
+
+	return 0
+}
+
+func initEnvironment(env Environment) error {
+	var err error
+
+	for name, val := range env {
+		if val.NeedRemove {
+			err = os.Unsetenv(name)
+		} else {
+			err = os.Setenv(name, val.Value)
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
